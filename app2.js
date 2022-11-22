7277c222d6d129030a88c1fe91e8cbe37c793551
Santiago Prediger <santiago.prediger@gmail.com>
2022-11-21 23:38:34 -0300

    agregado de objetos al carrito de compras

--- app2.js
+++ app2.js
@@ -1,3 +1,7 @@
+let botonInscribir = document.querySelectorAll('.botonInscribirse')
+const detalleTorneo = document.querySelector('#detalleTorneo')
+const detalleCosto = document.querySelector('#detalleCosto')
+
 // NOMBRE USUARIO
 
 const nombreUser = localStorage.getItem('nombreUsuario')
@@ -8,7 +12,7 @@ console.log(nombreUser, apellidoUser, emailUser)
 const bienvenida = document.querySelector('#nombreUser')
 
 bienvenida.innerHTML = `
-    <h2 class="tituloUser">Bienvenido ${nombreUser} ${apellidoUser}</h2>
+    <h2 class="tituloUser">Bienvenido ${nombreUser} y ${apellidoUser}</h2>
     <p class="email">Su email registrado es <a href="#">${emailUser}</a></p>
 `
 
@@ -27,16 +31,6 @@ const logoutUser = () => {
 
 logout.addEventListener('click', logoutUser)
 
-
-// VALIDAR DATOS LOCALSTORAGE
-
-/* if(nombreUser) {
-    bienvenida.innerHTML = `
-    <h2 class="tituloUser">Bienvenido ${nombreUser} ${apellidoUser}</h2>
-    <p class="email">Su email registrado es <a href="#">${emailUser}</a></p>
-`
-} else */
- 
 
 
 // CREACION DE TORNEOS 
@@ -61,12 +55,13 @@ function renderizarProductos(arrayTorneos) {
                 <i class="fa-solid fa-sort"></i><span class="categoria">Categoria ${torneo.categoria}</span><br>
                 <i class="fa-solid fa-calendar-days"></i><span class="dia">Dia ${torneo.dia}</span><br>
                 <i class="fa-solid fa-dollar-sign"></i></i><span class="precio"> Precio ${torneo.precio}</span></div>
-            <button class="botonInscribirse" id="${torneo.precio}"><a href="#" >Inscribirse</a></button>
+            <button class="botonInscribirse" id="${torneo.id}"><a href="#" >Inscribirse</a></button>
         </div>
     
         `
         torneos.append(nuevoTorneo)
     })
+    actualizarBotonInscribir();
 }
 renderizarProductos(listaTorneos);
 
@@ -82,17 +77,7 @@ listaProductos.forEach((producto) => {
         <h3>${producto.nombre}</h3>
         <p>1 unidad</p>
         <p>Precios: $${producto.precio}</p>
-        <form action="#" class="formularioBuscador">
-            <div class="seleccionCantidad">
-                <select name="cubregrip" class="cubregrip">
-                    <option value="">Selecciona una cantidad</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-                <button class="button" id="${producto.precio}">Agregar</button>
-            </div>
-        </form>
+        <button class="agregarProducto" id="${producto.id}">Agregar</button>
     `
     productos.append(nuevoProducto)
 })
@@ -139,37 +124,94 @@ const filtrarResultados = (e) => {
 filtrar.addEventListener('click', filtrarResultados)
 
 
-
+// LIMPIAR PARAMETROS DE BUSQUEDA TORNEOS
 
 const limpiarFiltros = document.querySelector('#limpiar')
 
 const renderizar = () => {
     renderizarProductos(listaTorneos)
 }
-
-
 limpiarFiltros.addEventListener('click', renderizar) 
 
 
-/* const torneos = document.querySelector('#torneos')
+// AGREGAR TORNEO AL CARRITO
 
-listaTorneos.forEach((torneo) => {
-    const nuevoTorneo = document.createElement('div')
-    nuevoTorneo.className = 'cardContainer'
-    nuevoTorneo.innerHTML = `
-    <div class="img">
-        <img src="${torneo.photo}" class="imgTorneo" alt="flyer torneo ${torneo.nombre}">
-    </div>
-    <div class="infoTorneo">
-        <h2>${torneo.nombre}</h2>
-        <div class="datosTorneo">
-            <i class="fa-solid fa-map-pin"></i><span class="lugar">${torneo.lugar}</span><br>
-            <i class="fa-solid fa-sort"></i><span class="categoria">Categoria ${torneo.categoria}</span><br>
-            <i class="fa-solid fa-calendar-days"></i><span class="dia">Dia ${torneo.dia}</span><br>
-            <i class="fa-solid fa-dollar-sign"></i></i><span class="precio"> Precio ${torneo.precio}</span></div>
-        <button class="botonInscribirse"><a href="#" >Inscribirse</a></button>
-    </div>
 
+
+
+function actualizarBotonInscribir() {
+    botonInscribir = document.querySelectorAll('.botonInscribirse')
+    botonInscribir.forEach((boton) => {
+        boton.addEventListener('click', torneoPagar);
+    })
+}
+
+const torneoEnCarrito = []
+const productosEnCarrito = [];
+
+function torneoPagar(e) {
+    const idBoton = e.currentTarget.id;
+    console.log(idBoton)
+    const torneoInscripto = listaTorneos.find((torneo) => torneo.id == idBoton)
+    console.log(torneoInscripto)
+
+    if(productosEnCarrito.some(torneo => torneo.id == idBoton)) {
+        alert('usted no se puede inscribir mas de una vez')
+    } else {
+        torneoEnCarrito.push(torneoInscripto)
+    }
+    
+    const detalleDetalle = document.createElement('div')
+    detalleDetalle.className = 'infoContainer'
+    detalleDetalle.innerHTML = `
+        <p>Inscripcion al torneo:${torneoEnCarrito[0].nombre}</p>
+        <p>En el club:${torneoEnCarrito[0].lugar} </p>
+        <p>El dia:${torneoEnCarrito[0].dia} </p>
+        <p>Jugador 1:${nombreUser}</p>
+        <p>Jugador 2:${apellidoUser}</p>
+        <p>Costo inscripcion:$${torneoEnCarrito[0].precio}</p>
     `
-    torneos.append(nuevoTorneo)
-}) */
+    detalleTorneo.append(detalleDetalle)
+    localStorage.setItem('torneoEnCarrito', JSON.stringify(torneoEnCarrito))
+
+}
+
+// AGREGAR PRODUCTOS AL CARRITO
+
+const agregarProducto = document.querySelectorAll('.agregarProducto')
+console.log(agregarProducto)
+agregarProducto.forEach(boton => {
+    boton.addEventListener('click', agregarAlCarrito)
+})
+
+function agregarAlCarrito(e) {
+    const id = e.currentTarget.id
+    const productoAgregado = listaProductos.find(producto => producto.id == id )
+    
+    if(productosEnCarrito.some(producto => producto.id == id)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id == id)
+        productosEnCarrito[index].cantidad++
+    } else {
+        productoAgregado.cantidad = 1
+        productosEnCarrito.push(productoAgregado)
+    }
+    localStorage.setItem('productosEnCarrito', JSON.stringify(productosEnCarrito))   
+    console.log(productosEnCarrito) 
+
+    const sumaCosto = document.createElement('div')
+    sumaCosto.className = 'sumaContainer'
+    sumaCosto.innerHTML = `
+        
+        <span>Cubregrips:</span><span>$</span>
+        <span>Protector de paleta:</span><span>$</span>
+        <span>Gorra:</span><span>$</span>
+        <span>TOTAL:</span><span>$</span>
+    `
+                    
+}
+
+
+
+
+
+
--- data.js
+++ data.js
@@ -1,27 +1,28 @@
 
 
 class Torneo {
-    constructor(nombre, lugar, categoria, dia, precio, photo) {
+    constructor(nombre, lugar, categoria, dia, precio, photo, id) {
         this.nombre = nombre;
         this.lugar = lugar;
         this.categoria = categoria;
         this.dia = dia;
         this.precio = precio;
         this.photo = photo;
+        this.id = id;
     }
 }
 
 // ARMADO DE OBJETOS
 
 
-const torneo1 = new Torneo('Ajpp', 'Matchpoint', 4 , 'Lunes', 1000, '../img/tor1.jpg');
-const torneo2 = new Torneo('New Green Padel', 'La Fabrica', 3 ,  'Martes', 2000, '../img/tor2.jpg');
-const torneo3 = new Torneo('Yuka Sports', 'Flow Sports', 2 ,  'Miercoles', 3000 ,'../img/tor3.jpg');
-const torneo4 = new Torneo('Chiqui Torneo', 'Efecto Padel', 1 , 'Jueves', 4000, '../img/tor4.jpg');
-const torneo5 = new Torneo('Ajpp', 'Matchpoint', 1 , 'Viernes', 1000, '../img/tor5.jpg');
-const torneo6 = new Torneo('New Green Padel', 'La Fabrica', 2 , 'Domingo', 2000, '../img/tor6.jpg');
-const torneo7 = new Torneo('Yuka Sports', 'Flow Sports', 3 , 'Sabado', 3000, '../img/tor7.jpg');
-const torneo8 = new Torneo('Chiqui Torneo', 'Efecto Padel', 4 , 'Domingo', 4000, '../img/tor8.jpg');
+const torneo1 = new Torneo('Ajpp', 'Matchpoint', 4 , 'Lunes', 1000, '../img/tor1.jpg',1);
+const torneo2 = new Torneo('New Green Padel', 'La Fabrica', 3 ,  'Martes', 2000, '../img/tor2.jpg',2);
+const torneo3 = new Torneo('Yuka Sports', 'Flow Sports', 2 ,  'Miercoles', 3000 ,'../img/tor3.jpg',3);
+const torneo4 = new Torneo('Chiqui Torneo', 'Efecto Padel', 1 , 'Jueves', 4000, '../img/tor4.jpg',4);
+const torneo5 = new Torneo('Ajpp', 'Matchpoint', 1 , 'Viernes', 1000, '../img/tor5.jpg',5);
+const torneo6 = new Torneo('New Green Padel', 'La Fabrica', 2 , 'Domingo', 2000, '../img/tor6.jpg',6);
+const torneo7 = new Torneo('Yuka Sports', 'Flow Sports', 3 , 'Sabado', 3000, '../img/tor7.jpg',7);
+const torneo8 = new Torneo('Chiqui Torneo', 'Efecto Padel', 4 , 'Domingo', 4000, '../img/tor8.jpg',8);
 
 
 
@@ -37,15 +38,16 @@ console.log(premioTorneo)
 
 
 class Producto {
-    constructor(photo, nombre, precio) {
+    constructor(photo, nombre, precio, id) {
         this.photo = photo;
         this.nombre = nombre;
         this.precio = precio;
+        this.id = id;
     }
 }
 
-const producto1 = new Producto('../img/cubregrips.jpg', 'Cubregrips', 800); 
-const producto2 = new Producto('../img/gorra.jpg', 'Gorra', 5000); 
-const producto3 = new Producto('../img/protectores.jpg', 'Protectores', 1200); 
+const producto1 = new Producto('../img/cubregrips.jpg', 'Cubregrips', 800, 'a'); 
+const producto2 = new Producto('../img/gorra.jpg', 'Gorra', 5000, 'b'); 
+const producto3 = new Producto('../img/protectores.jpg', 'Protectores', 1200, 'c'); 
 
 const listaProductos = [producto1, producto2, producto3]
